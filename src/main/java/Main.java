4ad9e312c2074b8c9ff556f9aecfd492e9a6bc9d
Evelin Szabados <szabadosevelin7@gmail.com>
2020-08-12 15:16:46 +0200

    Create class for db operations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        AppointmentGenerator appointmentGenerator = new AppointmentGenerator();
 
+        Scanner in = new Scanner(System.in);
+        System.out.println("Number of days to book appointments: ");
+        appointmentGenerator.createAppointment(in.nextInt());
+        in.close();
+        appointmentGenerator.listAppointments();
     }
 
 }
