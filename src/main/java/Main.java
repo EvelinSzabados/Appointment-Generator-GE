7af2e446e645a3e7a48f922e2d3747013231d316
Evelin Szabados <szabadosevelin7@gmail.com>
2020-08-12 16:06:30 +0200

    Handle user input in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,28 @@
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         AppointmentGenerator appointmentGenerator = new AppointmentGenerator();
-
         Scanner in = new Scanner(System.in);
-        System.out.println("Number of days to book appointments: ");
-        appointmentGenerator.createAppointment(in.nextInt());
+
+        while (true) {
+
+            try {
+                System.out.println("Number of days to book appointments: ");
+                String input = in.next();
+                appointmentGenerator.createAppointment(Integer.parseInt(input));
+
+                break;
+            } catch (NumberFormatException ne) {
+                System.out.println("Input is not a number, try again!");
+            }
+        }
+
+        System.out.println("Done!");
         in.close();
-        appointmentGenerator.listAppointments();
+
     }
 
 }
